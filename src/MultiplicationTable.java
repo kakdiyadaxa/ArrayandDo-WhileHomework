9a822afbb536a37edb5ebb0ae0a1ace7ee8d9978
Daxa <kakdiyadaxa@gmail.com>
2023-04-19 13:18:00 +0100

    1 added

--- src/MultiplicationTable.java
+++ src/MultiplicationTable.java
@@ -1,17 +1,24 @@
 public class MultiplicationTable {
-        public static void main(String[] args) {
+      public static void main(String[] args) {
             int i = 1, j;
             // using do-while loop to print multiplication table from 1 to 3
             do {
                 j = 1;
                 // using nested do-while loop to print the table for each number
                 do {
-                    System.out.print(i * j + " "); // print the product of i and j
-                    j++;
+                    System.out.print(i * j + "\t"); // print the product of i and j and tab character
+                   j++;
+                  //  System.out.println();
                 } while (j <= 3); // continue until the inner loop counter reaches 3
                 System.out.println(); // move to the next line
                 i++;
-            } while (i <= 3); // continue until the outer loop counter reaches 3
+                System.out.println();
+            } while (i <= 2); // continue until the outer loop counter reaches 3
+          int i1 = 3;
+          do {
+              System.out.print(i1 + " ");//print method for in same line
+              i1 += 3;
+          }while (i1 <= 9);
         }
     }
 
